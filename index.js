750b7e72d245964ebc9a272137d68d8572e81e02
Ben Martin <ben@benmartinstudios.com.au>
2017-02-14 18:30:03 +1000

    Fixed bug in node module; Updated to 0.2.2.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const iso_639_1 = require('data/iso_639_1');
-const iso_639_2 = require('data/iso_639_2');
+const iso_639_1 = require('./data/iso_639-1');
+const iso_639_2 = require('./data/iso_639-2');
 
 module.exports = {
   iso_639_1,
